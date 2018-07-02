a59c26337d6a1cf4e361c8a9a9d608be89ac5289
Adomkay <sh_adom@outlook.com>
2018-07-02 13:18:35 -0400

    Done.

--- driver.py
+++ driver.py
@@ -1,4 +1,10 @@
 # define Driver class and instance methods here
+class Driver:
+    def greeting(self):
+        return 'Hey, how are you?'
+
+    def ask_for_destination(self):
+        return 'Where would you like to go today?'
 
 # Note: remember that our instance methods need to be defined with the an
 # argument in order to be called on an instance of the class or else we will
